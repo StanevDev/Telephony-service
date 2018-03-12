602e4f7f0968c0026df848bf6087ae25db5e8cc8
Stanev Constantine <kostya.stanev@gmail.com>
2018-03-12 15:11:55 +0200

    TariffPlan entity added

---
+++ src/main/java/edu/jam/telephony/model/TariffPlan.java
@@ -0,0 +1,76 @@
+package edu.jam.telephony.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.sql.Date;
+
+public class TariffPlan implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+    private int         id;
+    private BigDecimal  price;
+    private Date        expiresDate;
+    private String      region;
+    private String      name;
+    private String      description;
+
+    public TariffPlan() {}
+
+    public TariffPlan(int id, BigDecimal price, Date expiresDate, String region, String name, String description) {
+        this.id = id;
+        this.price = price;
+        this.expiresDate = expiresDate;
+        this.region = region;
+        this.name = name;
+        this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public BigDecimal getPrice() {
+
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Date getExpiresDate() {
+        return expiresDate;
+    }
+
+    public void setExpiresDate(Date expiresDate) {
+        this.expiresDate = expiresDate;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
