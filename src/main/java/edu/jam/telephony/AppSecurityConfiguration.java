513062cc556185407413cf395862a9bd4fffcbf1
Constantine <kostya.stanev@gmail.com>
2018-05-16 05:16:59 +0300

    Basic auth for api layer added. It's 5 o'clock in the morning...

--- src/main/java/edu/jam/telephony/AppSecurityConfiguration.java
+++ src/main/java/edu/jam/telephony/AppSecurityConfiguration.java
@@ -1,26 +1,15 @@
 package edu.jam.telephony;
 
-import org.apache.logging.slf4j.SLF4JLogger;
-import org.apache.logging.slf4j.SLF4JLoggerContextFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
-import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -44,19 +33,37 @@ public class AppSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .passwordEncoder(new BCryptPasswordEncoder());
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest().authenticated()
-                    .and()
-                .formLogin()
-                    .loginPage("/login")
-                    .permitAll()
-                    .and()
-                .logout()
-                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                    .logoutSuccessUrl("/login?logout")
-                    .permitAll();
-    }
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .anyRequest().authenticated()
+//                    .and()
+//                .formLogin()
+//                    .loginPage("/login")
+//                    .permitAll()
+//                    .and()
+//                .logout()
+//                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+//                    .logoutSuccessUrl("/login?logout")
+//                    .permitAll();
+//    }
 
+    @Configuration
+    @Order(1)
+    public static class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+        @Autowired
+        BasicAuthenticationPoint point;
+
+        protected void configure(HttpSecurity http) throws Exception {
+//            http
+//                    .antMatcher("/api/**")
+//                    .authorizeRequests().anyRequest().authenticated()
+//                    .and()
+//                    .httpBasic();
+            http.csrf().disable();
+            http.authorizeRequests().antMatchers("/", "/api/**").permitAll()
+                    .anyRequest().authenticated();
+            http.httpBasic().authenticationEntryPoint(point);
+        }
+    }
 }
